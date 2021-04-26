69e3aac2d11627feda3ec0c9d093741e15ea751a
unknown <ef3d88@gmail.com>
2021-04-27 00:50:36 +0700

    catch exc

---
+++ src/main/java/com/eplacebo/javacore/chapter10/Exc2.java
@@ -0,0 +1,16 @@
+package com.eplacebo.javacore.chapter10;
+
+public class Exc2 {
+    public static void main(String[] args) {
+        int d, a;
+
+        try {
+            d=0;
+            a=42/d;
+            System.out.println("Не будет выведено!");
+        } catch (ArithmeticException e) {
+            System.out.println("Деление на ноль");
+        }
+        System.out.println("т.к. обработана, то это сообщение после catch.");
+    }
+}
