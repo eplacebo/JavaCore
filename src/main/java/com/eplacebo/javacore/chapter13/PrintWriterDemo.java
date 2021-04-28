485dae9a0af4b2f1ea0908ae46c8095ff2c856d5
unknown <ef3d88@gmail.com>
2021-04-28 14:32:29 +0700

    printwriter

---
+++ src/main/java/com/eplacebo/javacore/chapter13/PrintWriterDemo.java
@@ -0,0 +1,15 @@
+package com.eplacebo.javacore.chapter13;
+
+import java.io.PrintWriter;
+
+public class PrintWriterDemo {
+    public static void main(String[] args) {
+        PrintWriter pw = new PrintWriter(System.out, true);
+        pw.println("Строка");
+        int i = -7;
+        pw.println(i);
+        double d = 4.5e-7;
+        pw.println(d);
+    }
+}
+
