6a6ffadb30b7457d21b091e8970c01b23c18c68a
unknown <ef3d88@gmail.com>
2021-04-29 17:07:50 +0700

    instanceof

---
+++ src/main/java/com/eplacebo/javacore/chapter13/InstanceOf.java
@@ -0,0 +1,54 @@
+package com.eplacebo.javacore.chapter13;
+
+class A {
+    int i, j;
+}
+
+class B {
+    int i, j;
+}
+
+class C extends A {
+    int k;
+}
+
+class D extends A {
+    int k;
+}
+
+public class InstanceOf {
+    public static void main(String[] args) {
+        A a = new A();
+        B b = new B();
+        C c = new C();
+        D d = new D();
+
+        if (a instanceof A) System.out.println("а является экземпляром А");
+        if (b instanceof B) System.out.println("b является экземпляром B");
+        if (c instanceof C) System.out.println("c является экземпляром C");
+        if (c instanceof A) System.out.println("c можно привести к типу А");
+        if (a instanceof C) System.out.println("a можно привести к типу C");
+
+        System.out.println();
+
+        A ob;
+        ob = d;
+        System.out.println("ob теперь ссылается на d");
+
+        if (ob instanceof D) System.out.println("ob является экземпляром D");
+        System.out.println();
+
+        ob = c;
+        System.out.println("ob теперь ссылается на c");
+        if (ob instanceof D) System.out.println("ob можно привести к типу D");
+        else System.out.println("ob нельзя привести к типу D");
+
+        if (ob instanceof A) System.out.println("ob можно привести к типу A");
+        System.out.println();
+
+        if (a instanceof Object) System.out.println("а можн привести к типу Object");
+        if (b instanceof Object) System.out.println("b можн привести к типу Object");
+        if (c instanceof Object) System.out.println("c можн привести к типу Object");
+        if (d instanceof Object) System.out.println("d можн привести к типу Object");
+    }
+}
