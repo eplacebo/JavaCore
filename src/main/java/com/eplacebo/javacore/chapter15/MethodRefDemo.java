d884a7a36bdb16797b61267a3f9cf2557e4458d6
unknown <ef3d88@gmail.com>
2021-04-30 19:35:58 +0700

    ref to argument

---
+++ src/main/java/com/eplacebo/javacore/chapter15/MethodRefDemo.java
@@ -0,0 +1,29 @@
+package com.eplacebo.javacore.chapter15;
+
+class MyStringOps {
+    static String strReverse(String str) {
+        String result = "";
+        int i;
+
+        for (i = str.length() - 1; i >= 0; i--) {
+            result += str.charAt(i);
+        }
+        return result;
+    }
+}
+
+public class MethodRefDemo {
+    static String stringOp(StringFunc sf, String s) {
+        return sf.func(s);
+    }
+
+    public static void main(String[] args) {
+        String inStr = "Лямбда-выражения повышают эффективность Java";
+        String outStr;
+
+        outStr = stringOp(MyStringOps::strReverse, inStr);
+
+        System.out.println("Исходная строка: " + inStr);
+        System.out.println("Обращенная строка: " + outStr);
+    }
+}
