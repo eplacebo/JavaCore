ad7ba4ced1ad5744115f3cd3b6681a4ced7cc724
unknown <ef3d88@gmail.com>
2021-05-01 13:14:45 +0700

    util function interface

---
+++ src/main/java/com/eplacebo/javacore/chapter15/UseFunctionInterfaceDemo.java
@@ -0,0 +1,16 @@
+package com.eplacebo.javacore.chapter15;
+
+import java.util.function.Function;
+
+public class UseFunctionInterfaceDemo {
+    public static void main(String[] args) {
+        Function<Integer, Integer> factorial = n -> {
+            int result = 1;
+            for (int i = 1; i <= n; i++) {
+                result *= i;
+            }
+            return result;
+        };
+        System.out.println("Факториал 5 = " + factorial.apply(5));
+    }
+}
