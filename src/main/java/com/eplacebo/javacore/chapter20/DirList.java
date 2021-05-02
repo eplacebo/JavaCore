5ae25c2fa0eadd6d6386f850b629eee1d41f70be
unknown <ef3d88@gmail.com>
2021-05-02 12:55:01 +0700

    file filter

--- src/main/java/com/eplacebo/javacore/chapter20/DirList.java
+++ src/main/java/com/eplacebo/javacore/chapter20/DirList.java
@@ -6,14 +6,14 @@ public class DirList {
     public static void main(String[] args) {
         String dirname = "C:/Users/E/Desktop/Intro #181/Intro #181";
         File f1 = new File(dirname);
-        if (f1.isDirectory()){
-            System.out.println("Каталог "+dirname);
+        if (f1.isDirectory()) {
+            System.out.println("Каталог " + dirname);
             String s[] = f1.list();
 
             for (int i = 0; i < s.length; i++) {
-                File f = new File(dirname +"/" +s[i]);
+                File f = new File(dirname + "/" + s[i]);
 
-                if(f.isDirectory()) {
+                if (f.isDirectory()) {
                     System.out.println(s[i] + " является каталогом");
                 } else {
                     System.out.println(s[i] + " не является каталогом");
