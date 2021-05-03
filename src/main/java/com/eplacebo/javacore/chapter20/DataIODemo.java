d8498d8ce6e472eb113d4d2dc3f80dd754f4faf8
unknown <ef3d88@gmail.com>
2021-05-03 17:13:00 +0700

    data io

---
+++ src/main/java/com/eplacebo/javacore/chapter20/DataIODemo.java
@@ -0,0 +1,31 @@
+package com.eplacebo.javacore.chapter20;
+
+import java.io.*;
+
+public class DataIODemo {
+    public static void main(String[] args) {
+        try (DataOutputStream dout = new DataOutputStream(new FileOutputStream("C:\\Eplacebo\\java\\JavaCore\\src\\main\\java\\com\\eplacebo\\javacore\\chapter20\\Test.dat"))) {
+            dout.writeDouble(98.6);
+            dout.writeInt(1000);
+            dout.writeBoolean(true);
+        } catch (FileNotFoundException e) {
+            System.out.println("Нельзя открыть файл вывода");
+            return;
+        } catch (IOException e) {
+            System.out.println("Ошибка ввода-вывода: " + e);
+        }
+
+        try (DataInputStream din = new DataInputStream(new FileInputStream("C:\\Eplacebo\\java\\JavaCore\\src\\main\\java\\com\\eplacebo\\javacore\\chapter20\\Test.dat"))) {
+            double d = din.readDouble();
+            int i = din.readInt();
+            boolean b = din.readBoolean();
+
+            System.out.println("Получаемые значения: " + d + " " + i + " " + b);
+        } catch (FileNotFoundException e) {
+            System.out.println("Нельзя открыть файл ввода ");
+            return;
+        } catch (IOException e) {
+            System.out.println("Ошибка ввода-вывода: " + e);
+        }
+    }
+}
