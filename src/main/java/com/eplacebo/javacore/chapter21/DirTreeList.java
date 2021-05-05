bb4ac369df0c3ab7d7cc575879dbd3b5c50f191a
unknown <ef3d88@gmail.com>
2021-05-05 19:06:07 +0700

    walk file tree

---
+++ src/main/java/com/eplacebo/javacore/chapter21/DirTreeList.java
@@ -0,0 +1,25 @@
+package com.eplacebo.javacore.chapter21;
+
+import java.io.IOException;
+import java.nio.file.*;
+import java.nio.file.attribute.BasicFileAttributes;
+
+class MyFileVisitor extends SimpleFileVisitor<Path> {
+    public FileVisitResult visitFile(Path path, BasicFileAttributes attribs) throws IOException {
+        System.out.println(path);
+        return FileVisitResult.CONTINUE;
+    }
+}
+
+public class DirTreeList {
+    public static void main(String[] args) {
+        String dirname = "examples";
+        System.out.println("Дерево каталогов, начиная с каталога " + dirname + ":\n");
+
+        try {
+            Files.walkFileTree(Paths.get(dirname), new MyFileVisitor());
+        } catch (IOException exc) {
+            System.out.println("Ошибка ввода-вывода");
+        }
+    }
+}
