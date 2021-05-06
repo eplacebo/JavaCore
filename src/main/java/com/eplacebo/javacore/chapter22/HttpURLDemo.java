6862dad04abb31a34da118768e49f9e590f8ad19
unknown <ef3d88@gmail.com>
2021-05-06 14:47:50 +0700

    networking

---
+++ src/main/java/com/eplacebo/javacore/chapter22/HttpURLDemo.java
@@ -0,0 +1,29 @@
+package com.eplacebo.javacore.chapter22;
+
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+class HttpURLDemo {
+    public static void main(String[] args) throws IOException {
+        URL hp = new URL("http://www.google.com");
+        HttpURLConnection hpCon = (HttpURLConnection) hp.openConnection();
+
+        System.out.println("Метод запроса: " + hpCon.getRequestMethod());
+
+        System.out.println("Код ответа: " + hpCon.getResponseCode());
+
+        System.out.println("Ответное сообщение: " + hpCon.getResponseMessage());
+
+        Map<String, List<String>> hdrMap = hpCon.getHeaderFields();
+        Set<String> hdrField = hdrMap.keySet();
+        System.out.println("\nДалее следует заголовок:");
+
+        for (String k : hdrField) {
+            System.out.println("Ключ: " + k + " Значение: " + hdrMap.get(k));
+        }
+    }
+}
---
+++ src/main/java/com/eplacebo/javacore/chapter22/InetAdressTest.java
@@ -0,0 +1,19 @@
+package com.eplacebo.javacore.chapter22;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+public class InetAdressTest {
+    public static void main(String[] args) throws UnknownHostException {
+        InetAddress Address = InetAddress.getLocalHost();
+        System.out.println(Address);
+
+        Address = InetAddress.getByName("www.herbschildt.com");
+        System.out.println(Address);
+
+        InetAddress SW[] = InetAddress.getAllByName("www.nba.com");
+        for (int i = 0; i < SW.length; i++) {
+            System.out.println(SW[i]);
+        }
+    }
+}
